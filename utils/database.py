7c03032edbada5cac0483efea296c73fe7267e59
Arunkumar Shibu <86429222+arun017s@users.noreply.github.com>
2022-12-31 18:25:50 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #=========================================================================
 
-from .info import * 
+from . import * 
 from pymongo import MongoClient
 
 dbclient = MongoClient(DATABASE_URI)
